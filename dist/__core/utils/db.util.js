4d608fa68b5951eca4aea927333bb35e807a98da
Kolya <madridano.kolya@gmail.com>
2023-08-01 16:07:42 +0800

    fix(dist): update

--- dist/__core/utils/db.util.js
+++ dist/__core/utils/db.util.js
@@ -3,8 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
 const mongoose_1 = __importDefault(require("mongoose"));
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/db_swertesaya';
+const MONGODB_URI = process.env.ENVIRONMENT === "development"
+    ? process.env.DB_CONNECTION_STRING_LOCAL
+    : process.env.DB_CONNECTION_STRING;
 const connectDB = async () => {
     try {
         await mongoose_1.default.connect(MONGODB_URI);

a80f651d28684bad7e6a61b240d7d9d3ad5a80a4
Kolya <madridano.kolya@gmail.com>
2023-08-01 15:26:51 +0800

    fix(index): log env

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,18 @@ import betRoute from './routes/bet.route';
 import employeeRoute from './routes/employee.route';
 
 const app: Application = express();
+const envVars = {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
+    DB_CONNECTION_STRING_LOCAL: process.env.DB_CONNECTION_STRING_LOCAL,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_SECRET_NAME: process.env.AWS_SECRET_NAME,
+    BET_DOUBLE_NUM_LIMIT: process.env.BET_DOUBLE_NUM_LIMIT,
+    BET_TRIPLE_NUM_LIMIT: process.env.BET_TRIPLE_NUM_LIMIT,
+    BET_NORMAL_NUM_LIMIT: process.env.BET_NORMAL_NUM_LIMIT,
+    BET_RAMBLE_NUM_LIMIT: process.env.BET_RAMBLE_NUM_LIMIT,
+};
 
 // Middleware
 app.use(helmet()); // Apply standard security headers
@@ -27,6 +39,7 @@ connectDB();
 // Start the server
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
+    console.log('Environment Variables:', envVars);
 });
 
 export default app;

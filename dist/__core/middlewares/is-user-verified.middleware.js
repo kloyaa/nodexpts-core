7e8994d081501bb96491aa6be2e7d4e4a9e68158
Kolya <madridano.kolya@gmail.com>
2023-08-01 16:07:42 +0800

    fix(dist): update

--- dist/__core/middlewares/is-user-verified.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isUserVerified = void 0;
-const isUserVerified = async (req, res, next) => {
-    // const profile = await Profile.findOne({ user: req.user }).exec()
-    // console.log(profile)
-    // next();
-};
-exports.isUserVerified = isUserVerified;
--- dist/src/index.js
+++ dist/src/index.js
@@ -12,6 +12,18 @@ const profile_route_1 = __importDefault(require("./routes/profile.route"));
 const bet_route_1 = __importDefault(require("./routes/bet.route"));
 const employee_route_1 = __importDefault(require("./routes/employee.route"));
 const app = (0, express_1.default)();
+const envVars = {
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
+    DB_CONNECTION_STRING_LOCAL: process.env.DB_CONNECTION_STRING_LOCAL,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_SECRET_NAME: process.env.AWS_SECRET_NAME,
+    BET_DOUBLE_NUM_LIMIT: process.env.BET_DOUBLE_NUM_LIMIT,
+    BET_TRIPLE_NUM_LIMIT: process.env.BET_TRIPLE_NUM_LIMIT,
+    BET_NORMAL_NUM_LIMIT: process.env.BET_NORMAL_NUM_LIMIT,
+    BET_RAMBLE_NUM_LIMIT: process.env.BET_RAMBLE_NUM_LIMIT,
+};
 // Middleware
 app.use((0, helmet_1.default)()); // Apply standard security headers
 app.use((0, cors_1.default)()); // Enable CORS for all routes
@@ -21,10 +33,12 @@ app.use('/api', auth_route_1.default);
 app.use('/api', profile_route_1.default);
 app.use('/api', bet_route_1.default);
 app.use('/api', employee_route_1.default);
+app.get('/', (_, res) => res.send('Express Typescript on Vercel'));
 // Connect to MongoDB
 (0, db_util_1.default)();
 // Start the server
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
+    console.log('Environment Variables:', envVars);
 });
 exports.default = app;

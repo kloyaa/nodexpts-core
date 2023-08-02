4298541fe91d851abbd302aec961dd479c8df0ea
Kolya <madridano.kolya@gmail.com>
2023-08-02 23:56:17 +0800

    fix(dist): update to lastest

--- dist/src/routes/auth.route.js
+++ dist/src/routes/auth.route.js
@@ -7,5 +7,6 @@ const router = (0, express_1.Router)();
 router.post('/auth/v1/login', origin_middleware_1.checkUserOrigin, auth_controller_1.login);
 router.post('/auth/v1/register', origin_middleware_1.checkUserOrigin, auth_controller_1.register);
 router.post('/auth/v1/logout', auth_controller_1.logout);
+router.post('/auth/v1/token/verify', origin_middleware_1.checkUserOrigin, auth_controller_1.verifyToken);
 exports.default = router;
 //# sourceMappingURL=auth.route.js.map

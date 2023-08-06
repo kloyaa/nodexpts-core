2e9e4585b81b09fd40836b54cf5c267c24e4c075
Kolya <madridano.kolya@gmail.com>
2023-08-06 13:41:12 +0800

    fix(auth): use custom response

--- __core/const/api-statuses.const.ts
+++ __core/const/api-statuses.const.ts
@@ -97,4 +97,12 @@ export const statuses = {
         message: "Our server is currently undergoing maintenance to improve your experience. Please try again later.",
         code: "500",
     },
+    "10010": {
+        message: "Aws S3 JWT_SECRET is incorrect/invalid",
+        code: "10010",
+    },
+    "10020": {
+        message: "Authentication failed. Please login and try again.",
+        code: "10020",
+    }
 };

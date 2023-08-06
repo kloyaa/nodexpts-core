6db29c9f545aa7ad90936a1f75dce289eb5b9090
Kolya <madridano.kolya@gmail.com>
2023-08-07 01:28:29 +0800

    fix(util): set TZ for gtISODate

--- dist/__core/utils/date.util.js
+++ dist/__core/utils/date.util.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.gtISODate = void 0;
 const gtISODate = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');
-    const day = String(today.getDate()).padStart(2, '0');
+    const today = new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" });
+    const dateObject = new Date(today); // Convert the formatted string back to a Date object
+    const year = dateObject.getFullYear();
+    const month = String(dateObject.getMonth() + 1).padStart(2, "0");
+    const day = String(dateObject.getDate()).padStart(2, "0");
     const formattedDate = `${year}-${month}-${day}`;
     return formattedDate;
 };
